f492a88bc36f38476d0ef728439352e485fc0696
o0MURDOCK0o <dance_mit_de_gaense@gmx.de>
2022-04-21 16:53:04 +0200

    nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,9 @@
+vim.api.nvim_set_option("termguicolors", true) -- Enable 24-bit color
+vim.g.syntax = true -- Enable syntax highlighting
 
-
-
+-- Leader Key
+vim.g.mapleader = ' '
+vim.b.mapleader = ' '
 
 
 -- Global settings
@@ -50,3 +53,202 @@ vim.opt.listchars:append("eol:↴")
 vim.opt.scrolloff = 2 -- Lines of context
 vim.opt.sidescrolloff = 4 -- Columns of context
 vim.opt.signcolumn = "yes" -- Always show signcolumn
+
+-- Plugins
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- Nvim fennel integration
+    use 'rktjmp/hotpot.nvim'
+    -- Orgmode
+    use {
+        'nvim-neorg/neorg',
+        requires = 'nvim-lua/plenary.nvim',
+    }
+    -- Dracula colour scheme
+    use {
+        'Mofiqul/dracula.nvim',
+        config = function()
+            vim.cmd("colorscheme dracula")
+        end
+    }
+    -- Smart parentheses
+    use {
+        'eraserhd/parinfer-rust',
+        run = "cargo build --release",
+    }
+    -- Coloured parentheses
+    use {
+        'p00f/nvim-ts-rainbow',
+    }
+    -- Slime for vim
+    use {
+        'vlime/vlime',
+        --[[ config = function()
+        vim.api.nvim_exec("autocmd TextChangedI lisp call feedkeys(\"\\<c-x>\\<c-o>\")", false)
+        end, ]]
+    }
+    -- Show indent guides
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require("indent_blankline").setup {
+                char = "|",
+                space_char_blankline = " ",
+                show_current_context = true,
+                buftype_exclude = {"terminal"}
+            }
+        end
+    }
+    -- Treesitter support
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        config = function()
+            local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+                highlight = {
+                    enable = true,              -- false will disable the whole extension
+                },
+                indent = {
+                    enable = true
+                }
+            }
+        end,
+    }
+    use {
+        'neovim/nvim-lspconfig',
+    } -- Common configs for the in-built lsp client
+
+    use {
+        'hrsh7th/nvim-cmp',
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lsp_signature_help' },
+                    { name = 'neorg' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'omni' },
+                    { name = 'buffer' },
+                }),
+                sorting = {
+                    comparators = {
+                        cmp.config.compare.score,
+                    }
+                }
+            })
+        end,
+    } -- Completion
+    use {
+        'hrsh7th/cmp-nvim-lsp',
+        requires = 'hrsh7th/nvim-cmp',
+        requires = 'neovim/nvim-lspconfig',
+    }
+    use {
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        requires = 'hrsh7th/nvim-cmp',
+        requires = 'hrsh7th/cmp-nvim-lsp',
+        requires = 'neovim/nvim-lspconfig',
+    }
+    use {
+        'hrsh7th/cmp-buffer',
+        after = 'nvim-cmp',
+        requires = 'hrsh7th/nvim-cmp',
+    }
+    use {
+        'hrsh7th/cmp-path',
+        after = 'nvim-cmp',
+        requires = 'hrsh7th/nvim-cmp',
+    }
+    use {
+        'hrsh7th/cmp-omni',
+        after = 'nvim-cmp',
+        requires = 'hrsh7th/nvim-cmp',
+    }
+    use {
+        'saadparwaiz1/cmp_luasnip',
+        requires = 'hrsh7th/nvim-cmp',
+        requires = 'L3MON4D3/LuaSnip',
+    }
+
+    use {
+        'L3MON4D3/LuaSnip',
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    } -- Snippet Engine
+
+    use {
+        'rafamadriz/friendly-snippets',
+    } -- Snippets
+
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'} },
+    } -- Fuzzy Finding
+
+    use {
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup()
+        end,
+    } -- Auto pairs
+
+    use  {
+        'tpope/vim-repeat',
+    } -- Repeat commands by plugins
+
+    use {
+        'tpope/vim-surround',
+    } -- Surround text object
+
+    use {
+        'b3nj5m1n/kommentary',
+    } -- Comment out text
+
+    use {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end,
+    } -- Highlight color codes
+
+    use {
+        'editorconfig/editorconfig-vim',
+    } -- Editor config
+
+    use {
+        'beauwilliams/statusline.lua',
+    } -- Statusline
+
+    use {
+        'tpope/vim-fugitive',
+    } -- Vim git integration
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        },
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
+end)
